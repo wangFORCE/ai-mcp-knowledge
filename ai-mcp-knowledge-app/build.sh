f54025824add45ed76cc1e096b71546b50b83c7d
@2501 90125561 <2501_90125561@gitcode.net>
2026-03-20 20:41:44 +0800

    feat: mcp-server-csdn job

---
+++ ai-mcp-knowledge-app/build.sh
@@ -0,0 +1,3 @@
+# 普通镜像构建，随系统版本构建 amd/arm
+docker build -t wangbiggun/ai-mcp-knowledge-app:1.1 -f ./Dockerfile .
+
--- ai-mcp-knowledge-app/src/main/java/cn/wang/knowledge/config/OpenAIConfig.java
+++ ai-mcp-knowledge-app/src/main/java/cn/wang/knowledge/config/OpenAIConfig.java
@@ -5,8 +5,10 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.DefaultChatClientBuilder;
 import org.springframework.ai.chat.client.observation.ChatClientObservationConvention;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
 import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
@@ -64,5 +66,17 @@ public class OpenAIConfig {
     public ChatClient.Builder chatClientBuilder(OpenAiChatModel openAiChatModel) {
         return new DefaultChatClientBuilder(openAiChatModel, ObservationRegistry.NOOP, (ChatClientObservationConvention)null);
     }
+
+    @Bean
+    public ChatClient chatClient(OpenAiChatModel openAiChatModel, ToolCallbackProvider tools) {
+        DefaultChatClientBuilder defaultChatClientBuilder = new DefaultChatClientBuilder(openAiChatModel, ObservationRegistry.NOOP, (ChatClientObservationConvention) null);
+        return defaultChatClientBuilder
+                .defaultTools(tools)
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .model("gpt-4o")
+                        .build())
+                .build();
+    }
+
 
 }
